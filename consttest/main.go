27453ae9037a213da00537e7154f84194272dd30
xilin <1026771640@qq.com>
2019-11-01 17:01:28 +0800

    精简文件

--- consttest/main.go
+++ consttest/main.go
@@ -4,7 +4,7 @@ import "fmt"
 
 const (
 	Unknown = iota
-	Female  = "hahhha"
+	Female  = "ha"
 	Male
 	WECHAT = iota + 1
 	Alipay
@@ -18,13 +18,13 @@ const (
 )
 
 func main() {
-	fmt.Println(Unknown)
-	fmt.Println(Female)
-	fmt.Println(Male)
-	fmt.Println(WECHAT)
-	fmt.Println(Alipay)
-	fmt.Println("i=", i)
-	fmt.Println("j=", j)
-	fmt.Println("k=", k)
-	fmt.Println("l=", l)
+	fmt.Println(Unknown) //0
+	fmt.Println(Female)  //ha
+	fmt.Println(Male)    //ha 保留上一行数据
+	fmt.Println(WECHAT)  //4
+	fmt.Println(Alipay)  //5 保留上一行数据
+	fmt.Println("i=", i) //1
+	fmt.Println("j=", j) //6
+	fmt.Println("k=", k) //12
+	fmt.Println("l=", l) //24
 }
